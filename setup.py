1793f6bdb0b5ccb95751baa6867f83d6cf3d63ea
Pierre-Yves <pybaudin@gmail.com>
2022-08-12 11:38:43 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,5 @@ setup(
     packages=find_packages(),
     install_requires=["pydicom>=2.0"],
     entry_points={"console_scripts": ["dicom = dicomstack.cli:cli"]},
-    # metadata for upload to PyPI
-    author="Pierre-Yves Baudin",
-    author_email="py.baudin@cris-nmr.com",
     description="Convenient wrapper on pydicom for easy searching DICOM files",
-    license="",
-    url="https://bitbucket.org/pbaudin/dicomstack",
 )
