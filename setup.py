4e240daae458910cb4c0dc0385bdc77097cd8ba4
Pierre-Yves <pybaudin@gmail.com>
2023-11-22 11:31:40 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="dicomstack",
     version=__version__,
     packages=find_packages(),
-    install_requires=["pydicom>=2.0"],
+    install_requires=["pydicom>=2.4"],
     entry_points={"console_scripts": ["dicom = dicomstack.cli:cli"]},
     description="A pydicom wrapper for simple loading and handling of DICOM stacks ",
 )
