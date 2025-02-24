b334010f51cf60dfb550388ea972e52447a1de44
Pierre-Yves Baudin <py-baudin@institut-myologie.org>
2025-02-24 19:55:46 +0100

    fixed bug in pixel_data

--- dicomstack/pixeldata.py
+++ dicomstack/pixeldata.py
@@ -116,7 +116,7 @@ if AVAILABLE:
                 # if not same shape: drop metadata
                 return out_arr
             # else wrap out_array
-            return super().__array_wrap__(self, out_arr, context, return_scalar)
+            return super().__array_wrap__(out_arr, context, return_scalar)
 
 
 @available
